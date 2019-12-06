17ab560f4730e8f1aa20eea2f7d2f086d68148cf
rahls7 <sharma.rahul71993@gmail.com>
2019-12-06 14:47:40 -0500

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  resources :portfolios, except: [:show]
 
-  get 'about-me' to 'pages#about'
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
-  get 'contact' to 'pages#contact'
+  get 'about-me', to: 'pages#about'
+
+  get 'contact', to: 'pages#contact'
 
   resources :blogs
   
