9b60a836493328edf7e5aba1049040081754e6ff
rahls7 <sharma.rahul71993@gmail.com>
2019-12-07 22:56:52 -0500

    add defaults for image in portfolio

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,10 @@
 class Portfolio < ApplicationRecord
     validates_presence_of :title, :body, :main_image, :thumbnail
+
+    after_initialize :set_defaults
+
+    def set_defaults
+        self.main_image ||= "http://placehold.it/600x400"
+        self.thumbnail ||= "http://placehold.it/350x200"
+    end
 end
