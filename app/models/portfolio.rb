1d424061073567a9cd39a2e84ca5e256e026c150
rahls7 <sharma.rahul71993@gmail.com>
2019-12-08 02:02:33 -0500

    Nested attributes

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,7 +1,7 @@
 class Portfolio < ApplicationRecord
-    has_many :technologies
-    accepts_nested_technologies_for :technologies, 
-                                    reject_if: lambda {|attrs| attrs['name'].blank? }
+    has_many :technologies, inverse_of: :portfolio
+    accepts_nested_attributes_for :technologies, 
+                                    reject_if: lambda { |attrs| attrs['name'].blank? }
     validates_presence_of :title, :body, :main_image, :thumbnail
 
     include Placeholder
