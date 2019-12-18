d227c5a2199fa70f4c637f4e6c2ed2b4f64b1e1b
rahls7 <sharma.rahul71993@gmail.com>
2019-12-18 00:03:01 -0500

    Add petergate authorization for blog and portfolio

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -6,6 +6,11 @@ module CurrentUserConcern
   end
 
   def get_guest_user
-    OpenStruct.new(name: "Guest User", first_name: "Guest", last_name: "User", email: "guest@test.com")
+    guest = Guest.new
+    guest.name = "Guest User"
+    guest.first_name = "Guest"
+    guest.last_name = "User"
+    guest.email = "guest@example.com"
+    return guest
   end
 end
